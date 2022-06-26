aba4f69ea0a8c6e1d49ccd10fa8c0bd0133e5595
daardila10 <englishcadi2020@gmail.com>
2022-06-25 21:39:41 -0500

    my back1

---
+++ src/build/precache-manifest.e69b4b66b19aafe882c18b6e13217241.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e29faee4a56b030aded5",
+    "url": "/static/css/main.95b9d69b.chunk.css"
+  },
+  {
+    "revision": "e29faee4a56b030aded5",
+    "url": "/static/js/main.37b3f005.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f7125d3c337e9d104bda",
+    "url": "/static/js/2.f8b8cd58.chunk.js"
+  },
+  {
+    "revision": "78d79c6698302fe98fe4dac2449f75ce",
+    "url": "/index.html"
+  }
+];
